964116b0dced0afc60a007b25947e3a2a4a06312
Christian <Christianmnaira@gmail.com>
2024-12-16 20:47:10 +0800

    feat: added login user functionality

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,33 @@
 import java.util.HashMap;
+import java.util.Scanner;
 
 public class Main {
+
+    static void loginUser() {
+
+        Scanner askUser = new Scanner(System.in);
+
+        int i = 0;
+        while (i < 5) {
+
+            System.out.println("Enter your username: ");
+            String userName = askUser.nextLine();
+
+            if (!userName.isEmpty()) {
+
+                checkUserAccount();
+
+            } else {
+
+                System.out.println("Try again.");
+
+            }
+
+            i++;
+
+        }
+
+    }
 
     static void userAccounts() {
 
@@ -9,20 +36,19 @@ public class Main {
         userAccounts.put("Christian", 1234);
         userAccounts.put("Vincent", 1234);
 
-        for (String userAccount : userAccounts.keySet()) {
-
-            System.out.print(userAccount);
-            System.out.print(" ");
-            System.out.println(userAccounts.get(userAccount));
-
-        }
-
+//        for (String userAccount : userAccounts.keySet()) {
+//
+//            System.out.print(userAccount);
+//            System.out.print(" ");
+//            System.out.println(userAccounts.get(userAccount));
+//
+//        }
 
     }
 
     static void checkUserAccount() {
 
-
+        System.out.println("checking");
 
     }
 
@@ -31,6 +57,7 @@ public class Main {
         System.out.println("hello world");
 
         userAccounts();
+        loginUser();
 
     }
 }
