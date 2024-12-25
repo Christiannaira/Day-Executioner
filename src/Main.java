54df7152dfa9c208cdaf127316a05948cc171e1d
Christian <Christianmnaira@gmail.com>
2024-12-25 19:11:08 +0800

    feat: focusing on userHome component contents

--- src/Main.java
+++ src/Main.java
@@ -3,73 +3,46 @@ import java.util.Scanner;
 
 public class Main {
 
-    static void userHome(){
+    static void userHome(HashMap<String, Integer> UserAccounts){
 
-        System.out.println("Welcome! ");
+
 
     }
 
     static void loginUser(HashMap<String, Integer> userAccounts) {
-
         Scanner askUser = new Scanner(System.in);
-
         int i = 0;
         while (i < 5) {
-
             System.out.println("Enter your username: ");
             String userName = askUser.nextLine();
-
             if (!userName.isEmpty()) {
-
                 checkUserAccount(userAccounts, userName);
-
             } else {
-
                 System.out.println("Try again.");
-
             }
-
             i++;
-
         }
-
     }
 
     static void userAccounts(HashMap<String, Integer> userAccounts) {
-
         userAccounts.put("Christian", 1234);
         userAccounts.put("Vincent", 1234);
-
     }
 
     static void checkUserAccount(HashMap<String, Integer> userAccounts, String userName) {
-
         System.out.println("checking");
-
         Scanner askUserPassword = new Scanner(System.in);
-
         if (userAccounts.get(userName) != null) {
-
             System.out.println("Enter your password: ");
             int userPassword = askUserPassword.nextInt();
-
             if (userAccounts.containsValue(userPassword)) {
-
                 System.out.println("Logging in...");
-
             } else {
-
                 System.out.println("Incorrect Password ");
-
-
             }
-
         } else {
-
             System.out.println("no account was found");
-
         }
-
     }
 
     public static void main(String[] args) {
@@ -78,7 +51,8 @@ public class Main {
 
         System.out.println("hello world");
         userAccounts(userAccounts);
-        loginUser(userAccounts);
+//        loginUser(userAccounts);
+        userHome(userAccounts);
 
     }
 }
