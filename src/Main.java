148adc869691fad304a5a17759843e5d94fda3d7
Christian <Christianmnaira@gmail.com>
2024-12-26 11:02:44 +0800

    feat: added a data for october

--- src/Main.java
+++ src/Main.java
@@ -185,6 +185,16 @@ public class Main {
                 break;
 
             case "October":
+
+                System.out.println("\n\t\t\t\t\t==================================================\n");
+                System.out.println("\t\t\t\t\t=> " + month + "\n");
+                System.out.println("\t\t\t\t\t  S   \t  M   \t  T   \t  W   \t  T   \t  F   \t  S   \n");
+                System.out.println("\t\t\t\t\t      \t      \t      \t  1   \t  2   \t  3   \t  4   \n");
+                System.out.println("\t\t\t\t\t  5   \t  6   \t  7   \t  8   \t  9   \t  10   \t  11   \n");
+                System.out.println("\t\t\t\t\t  12   \t  13   \t  14   \t  15   \t  16   \t  17   \t  18   \n");
+                System.out.println("\t\t\t\t\t  19   \t  20   \t  21   \t  22   \t  23   \t  24   \t  25   \n");
+                System.out.println("\t\t\t\t\t  26   \t  27   \t  28   \t  29   \t  30   \t  31   \t       \n");
+                System.out.println("\t\t\t\t\t       \t       \t       \t       \t       \t       \t       \n");
                 break;
 
             case "November":
