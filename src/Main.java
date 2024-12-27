136bde4a242d188c040a7069d6a3a7bcdcc67ebc
Christian <Christianmnaira@gmail.com>
2024-12-27 11:33:22 +0800

    feat: added october calendar

--- src/Main.java
+++ src/Main.java
@@ -408,6 +408,17 @@ public class Main {
                 System.out.println("\t\t\t\t\t  19   \t  20   \t  21   \t  22   \t  23   \t  24   \t  25   \n");
                 System.out.println("\t\t\t\t\t  26   \t  27   \t  28   \t  29   \t  30   \t  31   \t       \n");
                 System.out.println("\t\t\t\t\t       \t       \t       \t       \t       \t       \t       \n");
+
+                int[] sundayListsOc = {5, 12, 19, 26};
+                int[] mondayListsOc = {6, 13, 20, 27};
+                int[] tuesdayListsOc = {7, 14, 21, 28};
+                int[] wednesdayListsOc = {1, 8, 15, 22, 29};
+                int[] thursdayListsOc = {2, 9, 16, 23, 30};
+                int[] fridayListsOc = {3, 10, 17, 24, 31};
+                int[] saturdayListsOc = {4, 11, 18, 25};
+
+                askDay(sundayListsOc, mondayListsOc, tuesdayListsOc, wednesdayListsOc, thursdayListsOc, fridayListsOc, saturdayListsOc);
+
                 break;
 
             case "November":
