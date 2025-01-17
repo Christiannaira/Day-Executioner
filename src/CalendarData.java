7124cb53aecb4555fea1dfa6475b4f5565f32d74
Christian <Christianmnaira@gmail.com>
2025-01-17 19:47:16 +0800

    fix: added the execution functionality to 2027 July Calendar

--- src/CalendarData.java
+++ src/CalendarData.java
@@ -1000,7 +1000,7 @@ public class CalendarData {
                 calendarJune.add("\t\t\t\t\t      \t      \t  1   \t  2   \t  3   \t  4   \t  5   \n");
                 calendarJune.add("\t\t\t\t\t  6   \t  7   \t  8   \t  9   \t  10   \t  11   \t  12   \n");
                 calendarJune.add("\t\t\t\t\t  13   \t  14   \t  15   \t  16   \t  17   \t  18   \t  19   \n");
-                calendarJune.add("\t\t\t\t\t  20   \t  21   \t  22   \t  23  \t   24   \t  25   \t  26   \n");
+                calendarJune.add("\t\t\t\t\t  20   \t  21   \t  22   \t  23   \t  24   \t  25   \t  26   \n");
                 calendarJune.add("\t\t\t\t\t  27   \t  28   \t  29   \t  30   \t       \t       \t       \n");
                 calendarJune.add("\t\t\t\t\t       \t       \t       \t       \t       \t       \t       \n");
 
@@ -1040,15 +1040,33 @@ public class CalendarData {
 
             case "July":
 
-                System.out.println("\n\t\t\t\t\t==================================================\n");
-                System.out.println("\t\t\t\t\t=> " + theMonth + "\n");
-                System.out.println("\t\t\t\t\t  S   \t  M   \t  T   \t  W   \t  T   \t  F   \t  S   \n");
-                System.out.println("\t\t\t\t\t      \t      \t      \t      \t  1   \t  2   \t  3   \n");
-                System.out.println("\t\t\t\t\t  4   \t  5   \t  6   \t  7   \t  8   \t  9   \t  10   \n");
-                System.out.println("\t\t\t\t\t  11   \t  12   \t  13   \t  14   \t  15   \t  16   \t  17   \n");
-                System.out.println("\t\t\t\t\t  18   \t  19   \t  20   \t  21  \t  22   \t  23   \t  24   \n");
-                System.out.println("\t\t\t\t\t  25   \t  26   \t  27   \t  28   \t  29   \t  30   \t  31   \n");
-                System.out.println("\t\t\t\t\t       \t       \t       \t       \t       \t       \t       \n");
+                ArrayList<String> calendarJuly = new ArrayList<>();
+                calendarJuly.add("\n\t\t\t\t\t==================================================\n");
+                calendarJuly.add("\t\t\t\t\t=> " + theMonth + "\n");
+                calendarJuly.add("\t\t\t\t\t  S   \t  M   \t  T   \t  W   \t  T   \t  F   \t  S   \n");
+                calendarJuly.add("\t\t\t\t\t      \t      \t      \t      \t  1   \t  2   \t  3   \n");
+                calendarJuly.add("\t\t\t\t\t  4   \t  5   \t  6   \t  7   \t  8   \t  9   \t  10   \n");
+                calendarJuly.add("\t\t\t\t\t  11  \t  12  \t  13  \t  14  \t  15   \t  16   \t  17   \n");
+                calendarJuly.add("\t\t\t\t\t  18  \t  19  \t  20  \t  21  \t  22   \t  23   \t  24   \n");
+                calendarJuly.add("\t\t\t\t\t  25  \t  26  \t  27  \t  28  \t  29   \t  30   \t  31   \n");
+                calendarJuly.add("\t\t\t\t\t       \t       \t       \t       \t       \t       \t       \n");
+
+                ArrayList<String> dateExecutedJuly = new ArrayList<>();
+
+
+//                System.out.println("\n\t\t\t\t\t==================================================\n");
+//                System.out.println("\t\t\t\t\t=> " + theMonth + "\n");
+//                System.out.println("\t\t\t\t\t  S   \t  M   \t  T   \t  W   \t  T   \t  F   \t  S   \n");
+//                System.out.println("\t\t\t\t\t      \t      \t      \t      \t  1   \t  2   \t  3   \n");
+//                System.out.println("\t\t\t\t\t  4   \t  5   \t  6   \t  7   \t  8   \t  9   \t  10   \n");
+//                System.out.println("\t\t\t\t\t  11   \t  12   \t  13   \t  14   \t  15   \t  16   \t  17   \n");
+//                System.out.println("\t\t\t\t\t  18   \t  19   \t  20   \t  21  \t  22   \t  23   \t  24   \n");
+//                System.out.println("\t\t\t\t\t  25   \t  26   \t  27   \t  28   \t  29   \t  30   \t  31   \n");
+//                System.out.println("\t\t\t\t\t       \t       \t       \t       \t       \t       \t       \n");
+
+                for (String july : calendarJuly) {
+                    System.out.println(july);
+                }
 
                 int[] sundayListsJy = {4, 11, 18, 25};
                 int[] mondayListsJy = {5, 12, 19, 26};
@@ -1058,7 +1076,13 @@ public class CalendarData {
                 int[] fridayListsJy = {2, 9, 16, 23, 30};
                 int[] saturdayListsJy = {3, 10, 17, 24, 31};
 
-//                askDay(sundayListsJy, mondayListsJy, tuesdayListsJy, wednesdayListsJy, thursdayListsJy, fridayListsJy, saturdayListsJy);
+                int countJuly = 0;
+                while (countJuly < 1000) {
+
+                    boolean singleValue = true;
+                    askDay(sundayListsJy, mondayListsJy, tuesdayListsJy, wednesdayListsJy, thursdayListsJy, fridayListsJy, saturdayListsJy, calendarJuly, singleValue, dateExecutedJuly);
+                    countJuly++;
+                }
 
                 break;
 
